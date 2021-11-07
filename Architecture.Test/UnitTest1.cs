9f526fbf1a698bcfc1014377c688bbaef6620ff7
Lars <larslehmann@kabelmail.de>
2021-11-07 15:02:47 +0100

    forgott stuff

---
+++ Architecture.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using ArchUnitNET.Loader;
+using ArchUnitNET.Fluent.Slices;
+using ArchUnitNET.NUnit;
+using Siteswaps;
+using static ArchUnitNET.Fluent.ArchRuleDefinition;
+namespace Architecture.Test
+{
+    public class Tests
+    {
+        private static readonly ArchUnitNET.Domain.Architecture Architecture =
+            new ArchLoader().LoadAssemblies(typeof(Siteswap).Assembly)
+                .Build();
+
+        [Test]
+        public void No_Dependencies_Between_High_Level_Folders()
+        {
+            SliceRuleDefinition.Slices().Matching("Siteswaps.(*)").Should().NotDependOnEachOther().Check(Architecture);
+        }
+    }
+}
