5d361d5007a0ea6f5de728391df24c0bd8897e9e
Lars <larslehmann@kabelmail.de>
2021-12-07 09:04:29 +0100

    first Blazor Unit Test

---
+++ Siteswaps.Components.Test/BunitTestContext.cs
@@ -0,0 +1,20 @@
+﻿using Bunit;
+using NUnit.Framework;
+using TestContext = Bunit.TestContext;
+
+namespace Siteswaps.Components.Test;
+
+public abstract class BunitTestContext : TestContextWrapper
+{
+    [SetUp]
+    public void Setup()
+    {
+        TestContext = new TestContext();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        TestContext?.Dispose();
+    }
+}
