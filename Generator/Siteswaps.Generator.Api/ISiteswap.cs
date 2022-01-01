9e3d8d56f880cfd00c9d5068ba4b2ed652fa0b09
Lars <larslehmann@kabelmail.de>
2022-01-01 22:12:47 +0100

    move to own file

---
+++ Generator/Siteswaps.Generator.Api/ISiteswap.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Immutable;
+
+namespace Siteswaps.Generator.Api;
+
+public interface ISiteswap
+{
+    public ImmutableList<int> Items { get; }
+}
--- Generator/Siteswaps.Generator.Api/ISiteswapGenerator.cs
+++ Generator/Siteswaps.Generator.Api/ISiteswapGenerator.cs
@@ -1,13 +1,6 @@
-﻿using System.Collections.Immutable;
-
-namespace Siteswaps.Generator.Api;
+﻿namespace Siteswaps.Generator.Api;
 
 public interface ISiteswapGenerator
 {
     Task<IEnumerable<ISiteswap>> GenerateAsync();
-}
-
-public interface ISiteswap
-{
-    public ImmutableList<int> Items { get; }
 }
