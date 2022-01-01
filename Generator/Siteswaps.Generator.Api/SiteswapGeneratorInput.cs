b77946eb1ff760685c9da40462a8211ac0c5a2be
Lars <larslehmann@kabelmail.de>
2022-01-01 22:19:05 +0100

    strg ef

--- Generator/Siteswaps.Generator.Api/SiteswapGeneratorInput.cs
+++ Generator/Siteswaps.Generator.Api/SiteswapGeneratorInput.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Siteswaps.Generator.Api;
+﻿namespace Siteswaps.Generator.Api;
 
 public record SiteswapGeneratorInput
 {
@@ -10,6 +9,7 @@ public record SiteswapGeneratorInput
         MinHeight = 2;
         NumberOfObjects = 7;
     }
+
     public SiteswapGeneratorInput(int period, int numberOfObjects, int minHeight, int maxHeight)
     {
         Period = period;
@@ -22,6 +22,7 @@ public record SiteswapGeneratorInput
     public int Period { get; init; } = 5;
     public int MinHeight { get; init; } = 2;
     public int MaxHeight { get; init; } = 10;
-    public StopCriteria StopCriteria { get; init; } = new (TimeSpan.FromSeconds(15), 1000);
+    public StopCriteria StopCriteria { get; init; } = new(TimeSpan.FromSeconds(15), 1000);
 }
+
 public record StopCriteria(TimeSpan TimeOut, int MaxNumberOfResults);
