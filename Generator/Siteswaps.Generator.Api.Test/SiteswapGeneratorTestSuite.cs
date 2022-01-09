8005f91b15b4d7b3a3b6799cf43982c2fa87dd79
Lars <larslehmann@kabelmail.de>
2022-01-09 02:28:24 +0100

    remove unnecessary test

--- Generator/Siteswaps.Generator.Api.Test/SiteswapGeneratorTestSuite.cs
+++ Generator/Siteswaps.Generator.Api.Test/SiteswapGeneratorTestSuite.cs
@@ -12,19 +12,22 @@ public abstract class SiteswapGeneratorTestSuite
     protected abstract ISiteswapGenerator CreateTestObject(SiteswapGeneratorInput input);
 
     [Test]
-    [TestCase(new[] { 5, 3, 1 })]
-    [TestCase(new[] { 4, 2, 3 })]
-    public async Task Generator_Generates_Siteswap(int[] expected)
+    [TestCase(3,  5, 0, 3)]
+    [TestCase(3, 13, 0, 8)]
+    [TestCase(3, 10, 0, 5)]
+    [TestCase(5, 10, 2, 7)]
+    [TestCase(5, 10, 2, 6)]
+    [TestCase(5,  5, 0, 3)]
+    [TestCase(7, 13, 2, 8)]
+    public async Task Verify_SiteswapGenerator_Against_Older_Version(int period, int maxHeight, int minHeight, int numberOfObjects)
     {
-        var input = Input(3, 5, 0, 3);
+        var input = Input(period, maxHeight, minHeight, numberOfObjects);
+        var sut = CreateTestObject(input);
 
-        var generator = CreateTestObject(input);
-
-
-        var result = (await generator.GenerateAsync()).ToList();
-        result.Should().Contain(expected);
+        var siteswaps = (await sut.GenerateAsync()).Select(x => x.ToString()).ToList();
+        await Verify(siteswaps).UseTypeName(nameof(SiteswapGeneratorTestSuite));
     }
-
+    
     private SiteswapGeneratorInput Input(int period, int maxHeight, int minHeight, int numberOfObjects)
     {
         return new SiteswapGeneratorInput
@@ -35,22 +38,5 @@ public abstract class SiteswapGeneratorTestSuite
             NumberOfObjects = numberOfObjects,
             StopCriteria = new(TimeSpan.FromSeconds(15), 10000)
         };
-    }
-
-    [Test]
-    [TestCase(5, 10, 2, 7)]
-    [TestCase(5, 10, 2, 6)]
-    [TestCase(7, 13, 2, 8)]
-    [TestCase(3, 13, 0, 8)]
-    [TestCase(3, 10, 0, 5)]
-    [TestCase(5, 5, 0, 3)]
-    public async Task Verify_SiteswapGenerator_Against_Older_Version(int period, int maxHeight, int minHeight, int numberOfObjects)
-    {
-        var input = Input(period, maxHeight, minHeight, numberOfObjects);
-        var sut = CreateTestObject(input);
-
-        var siteswaps = (await sut.GenerateAsync()).Select(x => x.ToString()).ToList();
-        await Verify(siteswaps).UseTypeName(nameof(SiteswapGeneratorTestSuite));
-
     }
 }
