84892bfc06846252c57885d44e420cfd89fc62f2
Lars <larslehmann@kabelmail.de>
2022-01-29 10:19:03 +0100

    add maui

---
+++ MauiRuntime/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiRuntime
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
---
+++ MauiRuntime/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using MauiRuntime.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Siteswaps.Generator.DependencyInjection;
+
+namespace MauiRuntime
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.RegisterBlazorMauiWebView()
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			builder.Services.AddBlazorWebView();
+			builder.Services.AddSingleton<WeatherForecastService>();
+			builder.Services.InstallGenerator();
+
+			return builder.Build();
+		}
+	}
+}
