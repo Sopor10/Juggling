69ed8948889255428e1b13d44146c115dc5a5779
Lars <larslehmann@kabelmail.de>
2022-06-12 16:45:45 +0200

    add Benchmark

---
+++ Generator/Benchmark/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Siteswaps.Generator.Api;
+using Siteswaps.Generator.Domain;
+using Siteswaps.Generator.Domain.Filter;
+
+var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+
+
+
+[MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
+public class GeneratorBenchmarks
+{
+
+    [Benchmark]
+    public void Generate()
+    {
+        var input = new SiteswapGeneratorInput()
+        {
+            Period = 5,
+            MaxHeight = 10,
+            MinHeight = 2,
+            NumberOfObjects = 7,
+            StopCriteria = new StopCriteria(TimeSpan.FromDays(5), Int32.MaxValue)
+        };
+        var generator = new SiteswapGeneratorFactory(new FilterBuilderFactory())
+            .WithInput(input)
+            .Create();
+        generator.GenerateAsync();
+    }
+}
