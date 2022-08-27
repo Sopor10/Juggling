a2e103e8b14f44da69b8803ba99951162f8880dc
Lars <larslehmann@kabelmail.de>
2022-08-28 00:00:48 +0200

    fix period bug

--- Generator/Siteswaps.Generator.Components/State/NewPatternFilterInformation.cs
+++ Generator/Siteswaps.Generator.Components/State/NewPatternFilterInformation.cs
@@ -1,10 +1,9 @@
 ﻿namespace Siteswaps.Generator.Components.State;
 
-public record NewPatternFilterInformation(List<Throw> Pattern, int Period, bool IsGlobalPattern) : IFilterInformation
+public record NewPatternFilterInformation(List<Throw> Pattern, bool IsGlobalPattern) : IFilterInformation
 {
-    public bool IsCompleted => false;
     public FilterType FilterType => FilterType.NewPattern;
-    public int MissingLength => Period - Pattern.Count;
+    public int MissingLength(int period) => period - Pattern.Count;
 
     public string Display() => "include " + (IsGlobalPattern ? "global " : "local ") + string.Join(" ", Pattern.Select(Display).ToList());
 
