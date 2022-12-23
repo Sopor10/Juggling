f59bd90e3f43a66d59921f746647ad20117ff3de
Lars <larslehmann@kabelmail.de>
2022-12-23 22:05:45 +0100

    create diagrams

---
+++ Siteswaps.Visualization/SVG/Renderer/StateNode.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Msagl.Core.Geometry;
+using Microsoft.Msagl.Core.Geometry.Curves;
+using Microsoft.Msagl.Drawing;
+
+namespace Siteswaps.Visualization.SVG.Renderer;
+
+public class StateNode : Node
+{
+    public StateNode(string id) : base(id)
+    {
+        Label = new Label(id);
+    }
+
+    private Label Label { get; }
+
+    public void CreateBoundary()
+    {
+        var height = Label.Height;
+        var width = 45d;
+
+        GeometryNode.BoundaryCurve = CurveFactory.CreateRectangle(width, height, new Point(0, 0));
+    }
+}
