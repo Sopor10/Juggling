d910b36741b0f6710057ea1ddca1d69a355fe44f
Lars <larslehmann@kabelmail.de>
2022-12-24 14:00:10 +0100

    add basic UI and add page to webassembly

--- Siteswap.Details/StateDiagram/StateGraph.cs
+++ Siteswap.Details/StateDiagram/StateGraph.cs
@@ -1,4 +1,6 @@
-﻿using Siteswap.Details.StateDiagram.Graph;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Siteswap.Details.StateDiagram.Graph;
 
 namespace Siteswap.Details.StateDiagram;
 
@@ -7,9 +9,20 @@ public class StateGraph
     public StateGraph(Graph<State, int> graph)
     {
         Graph = graph;
+        SiteswapInState = new();
+    }
+    
+    public StateGraph(Graph<State, int> graph, Dictionary<State, List<Siteswap>> siteswapInState)
+    {
+        Graph = graph;
+        SiteswapInState = siteswapInState;
     }
 
     public Graph<State, int> Graph { get; }
 
-    public StateGraph Combine(StateGraph other) => new(Graph.Combine(other.Graph));
+    public Dictionary<State, List<Siteswap>> SiteswapInState { get; }
+
+    public StateGraph Combine(StateGraph other) => new(Graph.Combine(other.Graph), SiteswapInState.Union(other.SiteswapInState).ToDictionary(x => x.Key, x => x.Value));
+    
+    
 }
