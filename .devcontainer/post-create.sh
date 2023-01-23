68b2bb4f21be7437ce64190934690e5c37777c8b
Lars Lehmann <lars.lehmann@smapone.com>
2023-01-23 09:04:55 +0100

    remove update from post create script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,11 +1,4 @@
-﻿## Install additional apt packages
-sudo apt-get update && \
-    sudo apt-get install -y dos2unix libsecret-1-0
-
-## Configure git
-git config --global core.autocrlf input
-
-## Enable local HTTPS for .NET
+﻿## Enable local HTTPS for .NET
 dotnet dev-certs https --trust
 
 dotnet workload install wasm-tools
