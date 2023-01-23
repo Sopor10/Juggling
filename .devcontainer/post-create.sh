1ab648a05ba5725555758eabe4a1150d173c5b73
Lars Lehmann <lars.lehmann@smapone.com>
2023-01-23 08:35:23 +0100

    add dev container

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,15 @@
+﻿## Install additional apt packages
+sudo apt-get update && \
+    sudo apt-get install -y dos2unix libsecret-1-0
+
+## Configure git
+git config --global core.autocrlf input
+
+## Enable local HTTPS for .NET
+dotnet dev-certs https --trust
+
+dotnet workload install wasm-tools
+## Restore .NET packages and build the default solution
+dotnet restore && dotnet build
+
+
