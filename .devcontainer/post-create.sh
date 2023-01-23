5e34f03f047e34aac89ac34dd36ce2e3dcf17b5d
Lars Lehmann <lars.lehmann@smapone.com>
2023-01-23 09:09:19 +0100

    delete file

--- .devcontainer/post-create.sh
+++
@@ -1,8 +0,0 @@
-﻿## Enable local HTTPS for .NET
-dotnet dev-certs https --trust
-
-dotnet workload install wasm-tools
-## Restore .NET packages and build the default solution
-dotnet restore && dotnet build
-
-
