55394015e4daa5c48bbc7e75f371398dbcb4043e
Sopor10 <45339903+Sopor10@users.noreply.github.com>
2023-07-26 11:42:34 +0200

    Revert "Change filter adding"

--- Generator/Siteswaps.Generator/Components/State/NewPatternFilterInformation.cs
+++ Generator/Siteswaps.Generator/Components/State/NewPatternFilterInformation.cs
@@ -1,10 +1,11 @@
 ﻿namespace Siteswaps.Generator.Components.State;
 
-public record NewPatternFilterInformation(IEnumerable<Throw> Pattern, bool IsGlobalPattern) : IFilterInformation
+public record NewPatternFilterInformation(List<Throw> Pattern, bool IsGlobalPattern) : IFilterInformation
 {
     public FilterType FilterType => FilterType.NewPattern;
+    public int MissingLength(int period) => period - Pattern.Count;
 
-    public string Display() => "include " + (IsGlobalPattern ? "global " : "local ") + string.Join(" ", Pattern.Reverse().SkipWhile(x => x == Throw.Empty).Reverse().Select(Display).ToList());
+    public string Display() => "include " + (IsGlobalPattern ? "global " : "local ") + string.Join(" ", Pattern.Select(Display).ToList());
 
-    private static string Display(Throw i) => i.DisplayValue;
+    private static string Display(Throw i) => i.Name;
 }
