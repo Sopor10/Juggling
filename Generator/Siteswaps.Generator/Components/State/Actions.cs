461d84c84c91402ccdb8ba5275297f2888931083
Lars Lehmann <lars.lehmann@smapone.com>
2024-09-22 17:19:50 +0200

    update packages

--- Generator/Siteswaps.Generator/Components/State/Actions.cs
+++ Generator/Siteswaps.Generator/Components/State/Actions.cs
@@ -4,9 +4,13 @@ using Siteswaps.Generator.Generator;
 namespace Siteswaps.Generator.Components.State;
 
 public record GenerateSiteswapsAction(GeneratorState State);
+
 public record PeriodChangedAction(Period? Value);
+
 public record ExactNumberChangedAction(int? Value);
+
 public record MinNumberChangedAction(int? Value);
+
 public record MaxNumberChangedAction(int? Value);
 
 public record NumberOfJugglersChangedAction(int? Value);
@@ -20,11 +24,17 @@ public record RemoveFilterNumber(int Value);
 public record ExactNumberOrRangeOfBallsSwitchedAction(bool Value);
 
 public record NewFilterCreatedAction(IFilterInformation Value);
+
 public record ChangedFilterAction(NewPatternFilterInformation NewPatternFilterInformation, int FilterNumber);
 
 public record SiteswapsGeneratedAction(IReadOnlyCollection<Siteswap> Siteswaps);
+
 public record FilterTypeSelectionChangedAction(FilterType FilterType);
+
 public record PatternFilterValueChangedAction(int Pos, int Value);
+
 public record SetState(GeneratorState State);
+
 public record ThrowsChangedAction(IEnumerable<Throw> Throws);
+
 public record CreateFilterFromThrowList(bool Value);
