c2ca6c3a35422d28ce27ec85a9ad1f0aa8eaf30f
Lars Lehmann <lars.lehmann@smapone.com>
2025-01-12 23:15:56 +0100

    remove old e2e test

--- Siteswaps.E2ETests/GenerateSiteswapsTests.cs
+++ Siteswaps.E2ETests/GenerateSiteswapsTests.cs
@@ -8,27 +8,6 @@ namespace Siteswaps.E2ETests;
 public class GenerateSiteswapsTests: E2ETestsBase
 {
     [Test]
-    public async Task Generator_UI_Generates_Siteswaps()
-    {
-        await Page.GotoAsync(ExpertUi);
-
-        await Page.ClickAsync("#generate");
-
-        await Page.Locator("//h3[normalize-space()='Siteswaps']").WaitForAsync();
-    }
-        
-    [Test]
-    public async Task EasyUI_Generates_Siteswaps()
-    {
-        await Page.GotoAsync(BaseUrl);
-
-        await Page.ClickAsync("#generate");
-        
-        await Page.Locator("//h3[normalize-space()='Siteswaps']").WaitForAsync();
-
-    }
-    
-    [Test]
     public async Task Fill_Out_Easy_Filter()
     {
         await Page.GotoAsync(BaseUrl);
@@ -62,5 +41,4 @@ public class GenerateSiteswapsTests: E2ETestsBase
 
 
     }
-    
 }
