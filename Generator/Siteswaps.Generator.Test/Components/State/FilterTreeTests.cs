011dcc74380524b3b135088aa3064cf7ede436f1
larsl <larslehmann10@gmail.com>
2025-01-28 08:36:10 +0100

    fix tests

--- Generator/Siteswaps.Generator.Test/Components/State/FilterTreeTests.cs
+++ Generator/Siteswaps.Generator.Test/Components/State/FilterTreeTests.cs
@@ -213,7 +213,7 @@ public class FilterTreeTests
         {
             CustomInstantiator(f => new NewPatternFilterInformation(
                 new List<Throw>(),
-                PatternRotation.Global,
+                PatternRotation.Local,
                 false
             ));
         }
