dd938662eaa98feb957dc67eed76478b2f2e7e05
larsl <larslehmann10@gmail.com>
2025-12-16 21:23:50 +0100

    save changes

--- Siteswaps.E2ETests/GeneratedSiteswapsResultPageObject.cs
+++ Siteswaps.E2ETests/GeneratedSiteswapsResultPageObject.cs
@@ -1,4 +1,4 @@
-﻿using FluentAssertions;
+using FluentAssertions;
 using Microsoft.Playwright;
 
 namespace Siteswaps.E2ETests;
@@ -14,5 +14,29 @@ public class GeneratedSiteswapsResultPageObject(IPage page)
     {
         var count = await page.GetByTestId($"generated-siteswap-{siteswap}").CountAsync();
         count.Should().Be(0);
+    }
+
+    public async Task<int> GetSiteswapCountAsync()
+    {
+        return await page.GetByTestId(new System.Text.RegularExpressions.Regex("generated-siteswap-.*")).CountAsync();
+    }
+
+    public async Task ShouldContainSiteswapAsync(string siteswap)
+    {
+        var element = page.GetByTestId($"generated-siteswap-{siteswap}");
+        await element.WaitForAsync();
+        var count = await element.CountAsync();
+        count.Should().BeGreaterThan(0);
+    }
+
+    public async Task ShouldGenerateAtLeastAsync(int minimumCount)
+    {
+        var count = await GetSiteswapCountAsync();
+        count.Should().BeGreaterOrEqual(minimumCount);
+    }
+
+    public async Task WaitForGenerationToCompleteAsync()
+    {
+        await page.WaitForSelectorAsync("text=Finished...", new() { Timeout = 60000 });
     }
 }
