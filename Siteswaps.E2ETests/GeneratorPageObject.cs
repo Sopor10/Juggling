e81e2528dfd6c4535b313439863aa24df366bee6
larsl <larslehmann10@gmail.com>
2025-12-16 21:23:50 +0100

    save changes

--- Siteswaps.E2ETests/GeneratorPageObject.cs
+++ Siteswaps.E2ETests/GeneratorPageObject.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Playwright;
+using Microsoft.Playwright;
 
 namespace Siteswaps.E2ETests;
 
@@ -8,6 +8,45 @@ public class GeneratorPageObject(IPage page)
     {
         await page.GetByTestId("generate-button").ClickAsync();
         return new(page);
+    }
+
+    public async Task<GeneratorPageObject> SetPeriodAsync(int period)
+    {
+        await page.GetByTestId("numeric-input-period").FillAsync(period.ToString());
+        await page.WaitForTimeoutAsync(100);
+        return this;
+    }
+
+    public async Task<GeneratorPageObject> SetNumberOfJugglersAsync(int numberOfJugglers)
+    {
+        await page.GetByTestId("numeric-input-numberOfJugglers")
+            .FillAsync(numberOfJugglers.ToString());
+        await page.WaitForTimeoutAsync(100);
+        return this;
+    }
+
+    public async Task<GeneratorPageObject> SetMinClubsAsync(int minClubs)
+    {
+        var minInput = page.Locator("rz-numeric").First;
+        await minInput.ClickAsync();
+        await minInput.Locator("input").FillAsync(minClubs.ToString());
+        await page.WaitForTimeoutAsync(100);
+        return this;
+    }
+
+    public async Task<GeneratorPageObject> SetMaxClubsAsync(int maxClubs)
+    {
+        var maxInput = page.Locator("rz-numeric").Nth(1);
+        await maxInput.ClickAsync();
+        await maxInput.Locator("input").FillAsync(maxClubs.ToString());
+        await page.WaitForTimeoutAsync(100);
+        return this;
+    }
+
+    public async Task<GeneratorPageObject> AddThrowAsync(string throwName)
+    {
+        await page.GetByTestId($"add-throw-{throwName}").ClickAsync();
+        return this;
     }
 
     public async Task<PatternFilterDialogObject> AddPatternFilterAsync(
@@ -24,5 +63,39 @@ public class GeneratorPageObject(IPage page)
         }
 
         return patternFilterDialogObject;
+    }
+
+    public async Task<NumberFilterDialogObject> AddNumberFilterAsync(
+        Func<NumberFilterDialogObject, Task>? configure = null
+    )
+    {
+        await page.GetByTestId("filter-node-AND").ClickAsync();
+        await page.GetByText("Add Filter").ClickAsync();
+        await page.GetByText("Number Filter").ClickAsync();
+
+        var numberFilterDialogObject = new NumberFilterDialogObject(page);
+        if (configure != null)
+        {
+            await configure(numberFilterDialogObject);
+        }
+
+        return numberFilterDialogObject;
+    }
+
+    public async Task<StateFilterDialogObject> AddStateFilterAsync(
+        Func<StateFilterDialogObject, Task>? configure = null
+    )
+    {
+        await page.GetByTestId("filter-node-AND").ClickAsync();
+        await page.GetByText("Add Filter").ClickAsync();
+        await page.GetByText("State Filter").ClickAsync();
+
+        var stateFilterDialogObject = new StateFilterDialogObject(page);
+        if (configure != null)
+        {
+            await configure(stateFilterDialogObject);
+        }
+
+        return stateFilterDialogObject;
     }
 }
